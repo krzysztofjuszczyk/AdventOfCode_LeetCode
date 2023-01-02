cf71f3a4022b352b9fd7224c90c49f28e6d348d3
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-01-02 19:51:46 +0100

    2020 package, day 1 part A

---
+++ src/aoc2020/D1.java
@@ -0,0 +1,35 @@
+package aoc2020;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class D1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/aoc2020/inputs/d1.txt"));
+        String line = reader.readLine();
+        Set<Integer> set = new HashSet();
+
+        while (line!= null){
+            int no = Integer.parseInt(line);
+            set.add(no);
+
+            if (no>350){
+                set.add(no);
+            }
+                else {
+                int remainder = 2020 - no;
+                if (set.contains(remainder)) {
+                    System.out.println(no + " * " + remainder + " = " + (no*remainder));
+                }
+
+
+            }
+            line = reader.readLine();
+        }
+
+
+    }
+}
---
+++ src/aoc2021/D3b.java
@@ -0,0 +1,71 @@
+package aoc2021;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class D3b {
+//    public static void main(String[] args) throws FileNotFoundException {
+//        int[] zeroes = new int[12];
+//        int[] ones = new int[12];
+//
+//        BufferedReader reader = new BufferedReader(new FileReader("src/aoc2021/inputs/d3.txt"));
+//        BufferedReader reader2 = new BufferedReader(new FileReader("src/aoc2021/inputs/d3.txt"));
+//        try {
+//            String line = reader.readLine();
+//            while (line!=null){
+//                char[] chars = line.toCharArray();
+//                for (int i = 0; i < 12; i++) {
+//                    if (chars[i] == '0'){
+//                        zeroes[i]++;
+//                    }
+//                    else ones[i]++;
+//                }
+//
+//
+//                line = reader.readLine();
+//            }
+//            System.out.println();
+//            String line2 = reader2.readLine();
+//            while (line2!= null){
+//                for (int i = 0; i < 12; i++) {
+//                    if (line2.charAt(i) = '??')
+//                }
+//
+//
+//                line2 = reader2.readLine();
+//            }
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String ogr = "";  //most common
+//        String csr = "";
+//        for (int i = 0; i < 12; i++) {
+//            if (ones[i] >= zeroes[i]){
+//                ogr += "1";
+//                csr += "0";
+//            }
+//            else {
+//                ogr += "0";
+//                csr += "1";
+//            }
+//
+//        }
+//        System.out.println("OGR:"+ ogr);
+//        int ogrVal = Integer.parseInt(ogr,2);
+//        System.out.println(ogrVal);
+//        System.out.println("CSR:"+ csr);
+//        System.out.println(ogrVal);
+//        int epsilonVal = Integer.parseInt(csr,2);
+//        int result = ogrVal*epsilonVal;
+//        System.out.println("result part 1:" + result);
+//
+//
+//
+//    }
+}
