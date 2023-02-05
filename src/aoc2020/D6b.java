a9f939507eac1ade1cc0aba839d0f6de93b1e6e6
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-02-05 11:25:11 +0100

    day 6 part B OK

--- src/aoc2020/D6b.java
+++ src/aoc2020/D6b.java
@@ -9,79 +9,52 @@ import java.util.Set;
 public class D6b {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("src/aoc2020/inputs/d6.txt"));
-        int result = 0;
-        int groupSize = 0;
-        int no = 1;
+        boolean groupCreated = false;
         Set<Character> tmpSet = new HashSet<>();
-        Set<Character> tmp2Set = new HashSet<>();
+        int result = 0;
+        int no =1;
         Set<Character> commonElements = new HashSet<>();
-        Set<Character> soloSet = new HashSet<>();
         try {
             String line = br.readLine();
             while (line != null) {
                 // end of GROUP
                 if (line.isEmpty()) {
-                    if (groupSize == 1) {
-                        result += soloSet.size();
-                        soloSet.clear();
-
-                    } else {
-                        System.out.println("group " + no + " Elements: " + commonElements);
-                        soloSet.clear();
-                        result += commonElements.size();
-                        groupSize = 0;
-
-                        commonElements.clear();
-                        no++;
-                    }
+                    result += commonElements.size();
+                    System.out.println("group " + no + " Elements: " + commonElements);
+                    no++;
+                    commonElements.clear();
+                    groupCreated = false;
+                    line = br.readLine();
+                    continue;
+//
                 }
-
-                groupSize++;
                 tmpSet.clear();
+
                 for (char c : line.toCharArray()) {
                     tmpSet.add(c);
                 }
 
-                if (groupSize == 1) {
-                    for (char c : line.toCharArray()) {
-                        soloSet.add(c);
-                    }
-
-                } else if (groupSize == 2) {
-                    commonElements = findCommonElements(soloSet, tmpSet);
-
-                } else {
-                    tmp2Set.clear();
-                    for (char ch : commonElements) {
-                        tmp2Set.add(ch);
-                    }
-                    commonElements.clear();
-                    commonElements = findCommonElements(tmp2Set, tmpSet);
+                if (commonElements.isEmpty() && !groupCreated) {
+                    commonElements.addAll(tmpSet);
+                    groupCreated = true;
+                }
+                else {
+                    commonElements.retainAll(tmpSet);
                 }
 
 
-
-
                 line = br.readLine();
-
-            }
+                }
 
             System.out.println("RESULT: " + result);
 
 
-    } catch(IOException ioException){
-        ioException.printStackTrace();
-    }}
+            // BAD results: 4380
 
-    public static Set<Character> findCommonElements(Set<Character> set1, Set<Character> set2) {
-        Set<Character> result = new HashSet<>();
-        for (Character s :
-                set1) {
-            if (set2.contains(s)) {
-                result.add(s);
-            }
-        }
-        return result;
-    }
+        } catch(IOException ioException){
+            ioException.printStackTrace();
+        }}
+
+
 
 }
