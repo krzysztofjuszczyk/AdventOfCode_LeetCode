b906bff7fe877f430e3294a2ad689da535d2e0bc
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-02-23 15:00:21 +0100

    2018 day 3 B 2nd version

--- src/aoc2018/D3.java
+++ src/aoc2018/D3.java
@@ -11,6 +11,7 @@ public class D3 {
         int[][] array = new int[1000][1000];
         int l = 0;
         int[] result = new int[1358];
+        boolean partBisOK = true;
 
         try {
 //            BufferedReader reader = new BufferedReader(new FileReader("src/aoc2018/inputs/d3test.txt"));
@@ -31,40 +32,71 @@ public class D3 {
                 for (int i = 0; i < w; i++) {
                     for (int j = 0; j < h; j++) {
                         if (array[row + i][col + j] != 0) {
-                            int x = array[row + i][col + j];
-                            result[l] = 1;
-                            result[x] = 1;
+                            // part B 1st ver
+//                            int x = array[row + i][col + j];
+//                            result[l] = 1;
+//                            result[x] = 1;
+
+                            // part B 2nd ver
+                            partBisOK = false;
                         }
-                        array[row + i][col + j] = l;
+                        array[row + i][col + j]++;
 
 //                        array[row+i][col+j]= l;   / for debugging
                     }
 
                 }
 
-
+//                if (partBisOK){
+//                    System.out.println(l);
+//            }
             }
 
 
-            for (int i = 0; i < result.length; i++) {
-                if (result[i] == 0) {
-                    System.out.println(i);
+// part B my 1st version -array of results
+//            for (int i = 0; i < result.length; i++) {
+//                if (result[i] == 0) {
+//                    System.out.println(i);
+//                }
+//            }
+
+            int res = 0;
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[0].length; j++) {
+                    if (array[i][j] > 1) {
+                        ++res;
+                    }
+//                    System.out.print(array[i][j]    );
+                }
+//                System.out.println();
+            }
+            System.out.println(res);
+
+           reader = new BufferedReader(new FileReader("src/aoc2018/inputs/d3.txt"));
+            l = 0;
+            while ((line = reader.readLine()) != null) {
+                partBisOK = true;
+                ++l;
+                String[] line2 = line.split("@");
+                String[] coordinates = line2[1].split(",");
+                int col = Integer.parseInt(coordinates[0].trim());
+                String[] tmp = coordinates[1].split(":");
+                int row = Integer.parseInt(tmp[0]);
+                tmp[1] = tmp[1].trim();
+                int h = Integer.parseInt(tmp[1].split("x")[0]);
+                int w = Integer.parseInt(tmp[1].split("x")[1]);
+                for (int i = 0; i < w; i++) {
+                    for (int j = 0; j < h; j++) {
+                        if (array[row + i][col + j] >1) {
+                            partBisOK = false;
+                        }
+                    }
+                }
+
+                if (partBisOK){
+                    System.out.println("PART B: " + l);
                 }
             }
-
-                    int res = 0;
-                    for (int i = 0; i < array.length; i++) {
-                        for (int j = 0; j < array[0].length; j++) {
-                            if (array[i][j] > 1) {
-                                ++res;
-                            }
-//                    System.out.print(array[i][j]    );
-                        }
-//                System.out.println();
-                    }
-                    System.out.println(res);
-
-
 
 
         } catch (FileNotFoundException e) {
@@ -72,5 +104,6 @@ public class D3 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
