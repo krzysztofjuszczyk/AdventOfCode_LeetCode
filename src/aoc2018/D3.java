56fd52e75f4c4159b5bd86f596c9f2767566487f
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-02-23 10:55:47 +0100

    2018 day 3 B solved

--- src/aoc2018/D3.java
+++ src/aoc2018/D3.java
@@ -8,46 +8,61 @@ import java.util.Arrays;
 
 public class D3 {
     public static void main(String[] args) {
-        int[] [] array = new int[1100][1100];
-        int l=0;
+        int[][] array = new int[1000][1000];
+        int l = 0;
+        int[] result = new int[1358];
 
         try {
 //            BufferedReader reader = new BufferedReader(new FileReader("src/aoc2018/inputs/d3test.txt"));
             BufferedReader reader = new BufferedReader(new FileReader("src/aoc2018/inputs/d3.txt"));
 
             String line;
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 ++l;
-                String [] line2 = line.split("@");
-                String [] coordinates = line2[1].split(",");
+                String[] line2 = line.split("@");
+                String[] coordinates = line2[1].split(",");
                 int col = Integer.parseInt(coordinates[0].trim());
-                String [] tmp = coordinates[1].split(":");
+                String[] tmp = coordinates[1].split(":");
                 int row = Integer.parseInt(tmp[0]);
-                tmp[1]= tmp[1].trim();
+                tmp[1] = tmp[1].trim();
                 int h = Integer.parseInt(tmp[1].split("x")[0]);
                 int w = Integer.parseInt(tmp[1].split("x")[1]);
 
                 for (int i = 0; i < w; i++) {
                     for (int j = 0; j < h; j++) {
-                        ++array[row+i][col+j];
+                        if (array[row + i][col + j] != 0) {
+                            int x = array[row + i][col + j];
+                            result[l] = 1;
+                            result[x] = 1;
+                        }
+                        array[row + i][col + j] = l;
 
 //                        array[row+i][col+j]= l;   / for debugging
                     }
 
                 }
 
+
             }
-            int result = 0;
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[0].length; j++) {
-                    if (array[i][j] > 1){
-                        ++result;
-                    }
-//                    System.out.print(array[i][j]    );
+
+
+            for (int i = 0; i < result.length; i++) {
+                if (result[i] == 0) {
+                    System.out.println(i);
                 }
-//                System.out.println();
             }
-            System.out.println(result);
+
+                    int res = 0;
+                    for (int i = 0; i < array.length; i++) {
+                        for (int j = 0; j < array[0].length; j++) {
+                            if (array[i][j] > 1) {
+                                ++res;
+                            }
+//                    System.out.print(array[i][j]    );
+                        }
+//                System.out.println();
+                    }
+                    System.out.println(res);
 
 
 
