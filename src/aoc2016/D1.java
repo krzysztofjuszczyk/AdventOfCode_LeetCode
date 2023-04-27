c794eed1f55b1b90c48697ed3d891621fa04c460
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-04-27 10:37:19 +0200

    AOC 2017 D1 part 2; moving on x,y coordinates using switch statements

--- src/aoc2016/D1.java
+++ src/aoc2016/D1.java
@@ -4,15 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static java.lang.Math.abs;
 
 public class D1 {
     public static void main(String[] args) {
         List<String> instructions = new ArrayList<>();
+        Set<Point> visited = new HashSet<>();
+        Point starting = new Point(0,0);
+        visited.add(starting);
 //        String[] directions = new String[]{"N", "E", "S", "W"}; // 0, 1, 2, 3
         int x =0;
         int y = 0;
@@ -41,23 +42,73 @@ public class D1 {
                 currentDir = (currentDir==3)? 0 : ++currentDir;
             }
 
-            switch (currentDir) {
+            for (int j = 0; j < howManyTimes; j++) {
+                switch (currentDir) {
                 case 0:
-                    y += howManyTimes;
+                    ++y;
                     break;
                 case 1:
-                    x += howManyTimes;
+                    ++x;
                     break;
                 case 2:
-                    y -= howManyTimes;
+                    --y;
                     break;
                 case 3:
-                    x -= howManyTimes;
+                    --x;
                     break;
             }
+            Point p = new Point(x,y);
+                if (visited.contains(p)){
+                    System.out.println("already visited point: "+ p.x + " " + p.y);
+                    System.out.println("distance:" + (abs(x) + abs(y)));
+                    break;
+                } else visited.add(p);
+            }
+
+
+//            //version counting only end points
+//            switch (currentDir) {
+//                case 0:
+//                    y += howManyTimes;
+//                    break;
+//                case 1:
+//                    x += howManyTimes;
+//                    break;
+//                case 2:
+//                    y -= howManyTimes;
+//                    break;
+//                case 3:
+//                    x -= howManyTimes;
+//                    break;
+//            }
+//            Point p = new Point(x,y);
+
+
         }
         int result = abs(x) + abs(y);
         System.out.println(result);
 
+    }
+
+    static class Point{
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x && y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        int x;
+        int y;
+        Point(int x, int y ){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
