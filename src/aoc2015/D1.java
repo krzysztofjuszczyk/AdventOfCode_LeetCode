a68b70e8008049b433ef38df5b76028a0e4a5061
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-06-01 18:39:00 +0200

    AOC 2015 d1

---
+++ src/aoc2015/D1.java
@@ -0,0 +1,21 @@
+package aoc2015;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class D1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/aoc2015/inputs2015/d1.txt"));
+        String line = reader.readLine();
+        int ans = 0;
+        for (int i=0; i<line.length(); i++){
+
+            if(line.charAt(i) == '('){
+                ans++;
+            } else ans--;
+        }
+        System.out.println(ans);
+    }
+}
