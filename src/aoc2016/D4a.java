0b9bff49ca53cdf7c3ff069e3404ea3dd55cf583
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-06-05 18:32:26 +0200

    AOC 2015 d4 in progress

--- src/aoc2016/D4a.java
+++ src/aoc2016/D4a.java
@@ -3,9 +3,7 @@ package aoc2016;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class D4a {
     public static void main(String[] args) throws IOException {
@@ -24,23 +22,50 @@ public class D4a {
             int code = Integer.parseInt(tmp2[0]);
             String verification = tmp2[1];
 //            System.out.println(code + " " + verification);
-
+            StringBuilder combined = new StringBuilder();
             for (int i = 0; i < split.length - 1; i++) {
                 String a = split[i];
-                for (int j = 0; j < a.length(); j++) {
-                    char c = a.charAt(j);
+                combined.append(a);
+            }
+
+            String combinedString = combined.toString();
+
+            for (int j = 0; j < combinedString.length(); j++) {
+                    char c = combinedString.charAt(j);
+
                     if (!map.containsKey(c)){
                         map.put(c, 1);
                     } else {
                         map.put(c, map.get(c) +1);
                     }
                 }
-            }
 
-            for (Map.Entry<Character,Integer> entry   : map.entrySet()
-                    ) {
+            ArrayList<Integer> list = new ArrayList<>();
+            LinkedHashMap<Character, Integer> sortedMap = new LinkedHashMap<>();
 
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                list.add(entry.getValue());
             }
+            Collections.sort(list);
+            for (int num : list) {
+                for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                    if (entry.getValue().equals(num)) {
+                        sortedMap.put(entry.getKey(), num);
+                    }
+                }
+            }
+            System.out.println(sortedMap);
+//sorted linked map in ascending order - now to reverse it
+
+            List<Character> allKeys = new ArrayList<>(sortedMap.keySet());
+            // reverse order of keys
+            Collections.reverse(allKeys);
+            //build a checksum
+            StringBuilder checksum = new StringBuilder();
+            for(Character strKey : allKeys){
+                checksum.append(strKey);
+            }
+            checksum.toString();
 
 
 
