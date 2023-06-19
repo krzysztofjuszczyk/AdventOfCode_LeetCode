1a7fa7a414434351ef36413a3a3a49f655132dba
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-06-19 10:02:00 +0200

    LeetCode: contains duplicate

---
+++ src/leetCode/easy/L217_ContainsDuplicate.java
@@ -0,0 +1,30 @@
+package leetCode.easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class L217_ContainsDuplicate {
+    //HashMap version
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return true;
+            }
+            map.put(nums[i],1);
+        }
+        return false;
+    }
+
+    // HashSet ver.
+    public boolean containsDuplicate2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
