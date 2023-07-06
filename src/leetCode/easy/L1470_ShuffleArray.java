27e3e57d54f75d2dfc4fa018b5e9fc968ea85384
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-07-06 08:48:51 +0200

    LC 1470 shuffle array

---
+++ src/leetCode/easy/L1470_ShuffleArray.java
@@ -0,0 +1,12 @@
+package leetCode.easy;
+
+public class L1470_ShuffleArray {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
+        }
+        return result;
+    }
+}
