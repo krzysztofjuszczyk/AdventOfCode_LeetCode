277940b9c6f321e71b1a1a653df6ddda98985a22
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-07-07 09:42:25 +0200

    LC 2469 convert temp

---
+++ src/leetCode/easy/L2469_ConvertTemperature.java
@@ -0,0 +1,8 @@
+package leetCode.easy;
+
+public class L2469_ConvertTemperature {
+    public double[] convertTemperature(double celsius) {
+            return new double[] {celsius+273.15,(celsius*1.8)+32}  ;
+        }
+    }
+
