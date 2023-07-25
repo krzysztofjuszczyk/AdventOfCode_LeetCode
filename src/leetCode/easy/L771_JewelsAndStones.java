3deed278251a249fac5ab6e55782dcee0a4aeb91
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-07-25 10:42:01 +0200

    LC 771 jewels and stones

--- src/leetCode/easy/L771_JewelsAndStones.java
+++ src/leetCode/easy/L771_JewelsAndStones.java
@@ -1,5 +1,6 @@
 package leetCode.easy;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,11 +13,32 @@ public class L771_JewelsAndStones {
         }
 
         for (int i = 0; i < stones.length(); i++) {
-            if(characterSet.contains(stones.charAt(i))){
+            if (characterSet.contains(stones.charAt(i))) {
                 ans++;
 //                characterSet.remove(stones.charAt(i));
             }
         }
         return ans;
+    }
+
+    public int numJewelsInStones2(String jewels, String stones) {
+        int res = 0;
+        for (char c : stones.toCharArray()) {
+            if (jewels.indexOf(c) != -1) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public int numJewelsInStones3(String jewels, String stones) {
+        int count = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < jewels.length(); i++)
+            map.put(jewels.charAt(i), map.getOrDefault(jewels.charAt(i) + 1, 1));
+        for (int i = 0; i < stones.length(); i++)
+            if (map.containsKey(stones.charAt(i)))
+                count++;
+        return count;
     }
 }
