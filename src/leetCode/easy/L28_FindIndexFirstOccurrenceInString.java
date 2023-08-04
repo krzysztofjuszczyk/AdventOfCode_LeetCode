5c560a320ffd091ed0037114002a421823f789ae
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-08-04 14:50:43 +0200

    LC 28

---
+++ src/leetCode/easy/L28_FindIndexFirstOccurrenceInString.java
@@ -0,0 +1,7 @@
+package leetCode.easy;
+
+public class L28_FindIndexFirstOccurrenceInString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
