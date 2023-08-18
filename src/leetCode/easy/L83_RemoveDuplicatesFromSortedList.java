6fce0b188b83018359351571cd18f1219b4017a2
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-08-18 17:54:28 +0200

    L83: Linked List, remove duplicates

---
+++ src/leetCode/easy/L83_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,22 @@
+package leetCode.easy;
+
+public class L83_RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode current = head;
+        ListNode next = current.next;
+
+        while (next != null){
+            if (next.val != current.val){
+                current.next = next;
+                current = next;
+            }
+               else { current.next = null;}
+
+                next = next.next;
+
+        }
+        return head;
+    }
+}
+
