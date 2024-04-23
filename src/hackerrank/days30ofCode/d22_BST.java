3c2fc4edf20894fb122b45551efab30f85671311
Krzysztof Juszczyk <kjuszczyk@gazeta.pl>
2024-04-23 14:35:08 +0200

    HackerRank: d22 BST

---
+++ src/hackerrank/days30ofCode/d22_BST.java
@@ -0,0 +1,23 @@
+package hackerrank.days30ofCode;
+
+public class d22_BST {
+    public static int getHeight(Node root){
+        //Write your code here
+        if (root == null) {return 0;}
+        else if ((root.left == null) & (root.right == null)){
+            return 0;
+        }
+        else return 1+ Math.max(getHeight(root.left), getHeight(root.right));
+
+    }
+
+    class Node{
+        Node left,right;
+        int data;
+        Node(int data){
+            this.data=data;
+            left=right=null;
+        }
+    }
+}
+
