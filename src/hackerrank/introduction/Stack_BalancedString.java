87525ab7fa1203986a7a5056e37df94cb90d2b78
Krzysztof Juszczyk <kjuszczyk@gazeta.pl>
2024-04-25 12:47:57 +0200

    HackerRank: balanced string 2 versions

--- src/hackerrank/introduction/Stack_BalancedString.java
+++ src/hackerrank/introduction/Stack_BalancedString.java
@@ -4,50 +4,72 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Stack_BalancedString {
-    public static void main(String []argh)
-    {
-        Scanner sc = new Scanner(System.in);
+    static boolean balancedString2(String s) {
+        // no additional space
+        while (true) {
+            int len = s.length();
+            s = s.replace("()", "");
+            s = s.replace("[]", "");
+            s = s.replace("{}", "");
+            int newlen = s.length();
+            if (len == newlen) {
+                break;
+            }
 
+        }
+        return s.length()==0;
+    }
 
+    static boolean balancedString(String s){
         String left = "{[(";
         String right = "}])";
 
-        while (sc.hasNext()) {
-            Stack<Character> st = new Stack<>();
-            String input=sc.next();
-            // System.out.println(input);
-            //Complete the code
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-                // System.out.println(c);
+        Stack<Character> st = new Stack<>();
 
-                // char is left
-                if (c == '{' || c =='(' || c == '[') {
-                    st.push(input.charAt(i));
-                }
+        // System.out.println(input);
+        //Complete the code
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // System.out.println(c);
 
-                // char is right
-                else {
-                    if (st.isEmpty()) {
-                        System.out.println("false");
-                        break;
-                    }
-                    char popped = st.pop();
-                    // System.out.println("popped "+popped);
-                    if (right.indexOf(c) != left.indexOf(popped)){
-                        System.out.println("false");}
-                }
+            // char is left
+            if (c == '{' || c =='(' || c == '[') {
+                st.push(s.charAt(i));
             }
 
-            // debugging
-            // for (int i = 0; i < st.size(); i++) {
-            //     System.out.print(st.pop());
-            // }
-            // System.out.println(st.size());
-            if (st.size() == 0){
-                System.out.println("true");}
-            else System.out.println("false");
+            // char is right
+            else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char popped = st.pop();
+                // System.out.println("popped "+popped);
+                if (right.indexOf(c) != left.indexOf(popped)){
+                    return false;}
+            }
+        }
+
+        // debugging
+        // for (int i = 0; i < st.size(); i++) {
+        //     System.out.print(st.pop());
+        // }
+        // System.out.println(st.size());
+        if (st.size() == 0){
+            return true;}
+        else return false;
+
+    }
+
+
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input=sc.next();
+            String ans = balancedString(input) == true ? "true" : "false";
+            System.out.println(ans);
 
         }
     }
 }
+
